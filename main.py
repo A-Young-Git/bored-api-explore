e8af5dd96562855bb04d5805e9b90d7620ebc7da
Tony <tony3159292@gmail.com>
2025-08-29 15:28:43 -0400

    refactored the kidfriendly function to use polars more effectively and adjusted variable name that calls that function to be more intuitive

--- main.py
+++ main.py
@@ -4,33 +4,31 @@ import polars as pl
 BASE_URL = "https://bored-api.appbrewery.com"
 
 
-def get_random_activity(BASE_URL: str) -> str:
-    request = requests.get(f"{BASE_URL}/random")
-    response = request.json()
+def get_random_activity(base_url: str) -> str:
+    response = requests.get(f"{base_url}/random").json()
 
     return response.get("activity")
 
 
 def get_kid_friendly_activities(
-    BASE_URL: str, activity_type: str, participants: int
+    base_url: str, activity_type: str, participants: int
 ) -> int:
-    request = requests.get(
-        f"{BASE_URL}/filter?type={activity_type}&participants={participants}"
-    )
-    response = request.json()
+    response = requests.get(
+        f"{base_url}/filter?type={activity_type}&participants={participants}"
+    ).json()
 
     # Convert to Polars dataframe
     data = pl.DataFrame(response)
-    kid_friendly_activities = data.get_column("kidFriendly").count()
-
-    return kid_friendly_activities
+    return data.filter(pl.col("kidFriendly")).shape[0]
 
 
-# random_activity = get_random_activity(BASE_URL)
-# print(f'3..2..1..You\'re randomly chosen activity is "{random_activity}."')
+random_activity = get_random_activity(BASE_URL)
+print(f'3..2..1..You\'re randomly chosen activity is "{random_activity}."\n')
 
 
-kid_friendly_activities = get_kid_friendly_activities(
+kid_friendly_activities_count = get_kid_friendly_activities(
     BASE_URL, activity_type="education", participants=1
 )
-print(f"There are {kid_friendly_activities} kid friendly activities in this dataset.")
+print(
+    f"There are {kid_friendly_activities_count} kid friendly activities in this dataset."
+)
