a42e2d697e322ac91ad202a6d6670d7107a57737
Tony <tony3159292@gmail.com>
2025-08-29 15:10:44 -0400

    Added kid friendly activities function and played with polar to create dataframe and count up values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import polars as pl
 
 BASE_URL = "https://bored-api.appbrewery.com"
 
@@ -10,6 +11,26 @@ def get_random_activity(BASE_URL: str) -> str:
     return response.get("activity")
 
 
-activity = get_random_activity(BASE_URL)
+def get_kid_friendly_activities(
+    BASE_URL: str, activity_type: str, participants: int
+) -> int:
+    request = requests.get(
+        f"{BASE_URL}/filter?type={activity_type}&participants={participants}"
+    )
+    response = request.json()
 
-print(f'3..2..1..You\'re randomly chosen activity is "{activity}."')
+    # Convert to Polars dataframe
+    data = pl.DataFrame(response)
+    kid_friendly_activities = data.get_column("kidFriendly").count()
+
+    return kid_friendly_activities
+
+
+# random_activity = get_random_activity(BASE_URL)
+# print(f'3..2..1..You\'re randomly chosen activity is "{random_activity}."')
+
+
+kid_friendly_activities = get_kid_friendly_activities(
+    BASE_URL, activity_type="education", participants=1
+)
+print(f"There are {kid_friendly_activities} kid friendly activities in this dataset.")
